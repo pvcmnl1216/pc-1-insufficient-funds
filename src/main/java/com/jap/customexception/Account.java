170a49f4dfaaf83f566df96b0812d37d8d08ba84
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-10-13 16:22:27 +0530

    Completed

--- src/main/java/com/jap/customexception/Account.java
+++ src/main/java/com/jap/customexception/Account.java
@@ -1,7 +1,6 @@
 package com.jap.customexception;
 
 public class Account {
-
     private int accountBalance;
 
     public Account(int accountBalance) {
@@ -9,7 +8,6 @@ public class Account {
     }
 
     //create getter and setter
-
 
     public int getAccountBalance() {
         return accountBalance;
@@ -19,22 +17,11 @@ public class Account {
         this.accountBalance = accountBalance;
     }
 
-    /**
-     * This method returns the new balance deducting the withdraw amount from the balance
-     * Throws InsufficientFundException when amount to withdraw is greater than the balance
-     *
-     * @param amount
-     * @return
-     * @throws InsufficientFundException
-     */
     public int withdraw(int amount) throws InsufficientFundException {
         if (amount < accountBalance) {
             return accountBalance - amount;
         } else {
             throw new InsufficientFundException("Insufficient Balance");
         }
-
     }
-
-
 }
